c0761e42ae9456271324e5fdc8df56de7e00cf78
Ingmar.kodehode <ingmar.kodehode@gmail.com>
2023-06-12 11:24:08 +0200

    pull

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/Ingmarkodehode/react-oppgave-1.git",
   plugins: [react()],
 })
